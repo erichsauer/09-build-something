253a054dc41b8123396bc70e37d84913265669d2
Erich Sauer <erich.sauer@gmail.com>
2021-04-08 09:03:11 -0700

    animal post route is green!

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,73 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('09-build-something routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
-
-  let testPlant;
-  beforeEach(async () => {
-    const { body } = await request(app).post('/api/v1/plants').send({
-      name: 'planty',
-      favoriteSeason: 'spring',
-      flowering: true,
-      zone: 9,
-    });
-    testPlant = body;
-  });
-
-  it('should add a plant when hit', async () => {
-    const res = await request(app).post('/api/v1/plants').send({
-      name: 'flower pal',
-      favoriteSeason: 'summer',
-      flowering: true,
-      zone: 7,
-    });
-    expect(res.body).toEqual({
-      id: expect.any(String),
-      name: 'flower pal',
-      favoriteSeason: 'summer',
-      flowering: true,
-      zone: 7,
-    });
-  });
-
-  it('should retrieve all plants', async () => {
-    const res = await request(app).get(`/api/v1/plants`);
-    expect(res.body[0]).toEqual(testPlant);
-  });
-
-  it('should retrieve one plant by id', async () => {
-    const res = await request(app).get(`/api/v1/plants/${testPlant.id}`);
-    expect(res.body).toEqual(testPlant);
-  });
-
-  it('should update a plant by id', async () => {
-    const res = await request(app).put(`/api/v1/plants/${testPlant.id}`).send({
-      name: 'Plantsy',
-      favoriteSeason: 'autumn',
-      flowering: false,
-      zone: 9,
-    });
-    expect(res.body).toEqual({
-      id: testPlant.id,
-      name: 'Plantsy',
-      favoriteSeason: 'autumn',
-      flowering: false,
-      zone: 9,
-    });
-  });
-
-  it('should delete a plant by id', async () => {
-    const res = await request(app).delete(`/api/v1/plants/${testPlant.id}`);
-    expect(res.body).toEqual(testPlant);
-  });
-});
 describe('plant routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+  beforeEach(() => setup(pool));
 
   let testPlant;
   beforeEach(async () => {
@@ -135,7 +70,7 @@ describe('animal routes', () => {
 
   let testAnimal;
   beforeEach(async () => {
-    const { body } = await (await request(app).post('api/v1/animals')).send({
+    const { body } = await request(app).post('/api/v1/animals').send({
       name: 'critter',
       dayOrNight: 'night',
       furry: true,
@@ -145,7 +80,7 @@ describe('animal routes', () => {
   });
 
   it('should add an animal', async () => {
-    const res = await (await request(app).post('api/v1/animals')).send({
+    const res = await request(app).post('/api/v1/animals').send({
       name: 'skipper',
       dayOrNight: 'day',
       furry: false,
@@ -154,9 +89,11 @@ describe('animal routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       name: expect.any(String),
-      dayOrNight: expect.toEqual('day' || 'night'),
+      dayOrNight: expect.any(String),
       furry: expect.any(Boolean),
       numberOfLegs: expect.any(Number),
     });
   });
 });
+
+afterAll(() => pool.end());
